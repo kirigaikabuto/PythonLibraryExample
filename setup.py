b507e4afeb55b4429af1fcff9ba9854d0e46eacf
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-08-03 16:50:44 +0600

    redeploy

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(name='library1',
+      version='0.1',
+      description='What the module does',
+      url='https://github.com/kirigaikabuto/PythonLibraryExample',
+      author='KirigaiKabuto',
+      author_email='tleugazy98@gmail.com',
+      license='MIT',
+      packages=['library1'],
+      install_requires=['numpy>=1.11',
+                        'matplotlib>=1.5'])
